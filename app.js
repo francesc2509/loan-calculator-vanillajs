976cf8865fb0bd91c6795ab921ab6718ef68758e
Francesc Manarel Bueno <francesc2509@outlook.com>
2021-09-26 18:35:39 +0200

    Add loading functionality

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const loanForm = document.getElementById('loan-form');
-  const loadingDiv = document.getElementById('loading');
-
   loanForm.addEventListener('submit', calculate);
 
-  const calculationResultDiv = document.getElementById('calculation-result');
-
-  const monthlyPmtInput = calculationResultDiv.querySelector("#monthly-pmt");
-  const totalPmtInput = calculationResultDiv.querySelector("#total-pmt");
-  const totatlInterestInput = calculationResultDiv.querySelector("#total-interest");
+  const loadingDiv = document.getElementById('loading');
+  let calculationResultDiv;
 
   function calculate(event) {
-    console.log('Calculate');
+    calculationResultDiv = document.getElementById('calculation-result');
+    calculationResultDiv.classList.add('d-none');
 
-    const principal = parseFloat(loanForm.amount.value);
-    const calculatedInterest = parseFloat(loanForm.interest.value) / 100 / 12;
-    const calculatedPmts = parseFloat(loanForm.years.value) * 12;
+    showLoading(() => {
+      const monthlyPmtInput = calculationResultDiv.querySelector("#monthly-pmt");
+      const totalPmtInput = calculationResultDiv.querySelector("#total-pmt");
+      const totatlInterestInput = calculationResultDiv.querySelector("#total-interest");
+      const principal = parseFloat(loanForm.amount.value);
+      const calculatedInterest = parseFloat(loanForm.interest.value) / 100 / 12;
+      const calculatedPmts = parseFloat(loanForm.years.value) * 12;
 
-    const x = Math.pow(1 + calculatedInterest, calculatedPmts)
-    const monthly = (principal * x * calculatedInterest)/(x - 1);
-    if (isFinite(monthly)) {
-      monthlyPmtInput.value = monthly.toFixed(2);
-      totalPmtInput.value = (monthly*calculatedPmts).toFixed(2);
-      totatlInterestInput.value = ((monthly * calculatedPmts) - principal).toFixed(2);
-    } else {
-      showError('Please, check your numbers') 
-    }
+      const x = Math.pow(1 + calculatedInterest, calculatedPmts)
+      const monthly = (principal * x * calculatedInterest)/(x - 1);
+      if (isFinite(monthly)) {
+        monthlyPmtInput.value = monthly.toFixed(2);
+        totalPmtInput.value = (monthly*calculatedPmts).toFixed(2);
+        totatlInterestInput.value = ((monthly * calculatedPmts) - principal).toFixed(2);
 
-    event.preventDefault();
+        calculationResultDiv.classList.remove('d-none');
+      } else {
+        showError('Please, check your numbers') 
+      }
+    });
+
+    return event.preventDefault();
   }
 
   function showError(errMsg) {
@@ -37,6 +40,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
     loanForm.appendChild(errDiv);
 
     setTimeout(() => loanForm.removeChild(errDiv), 3000);
+  }
+
+  function showLoading(callback) {
+    loadingDiv.classList.remove('d-none');
+    loanForm.sbmt.disabled = true;
+
+    setTimeout(() => {
+      loanForm.sbmt.disabled = false;
+      loadingDiv.classList.add('d-none');
+      callback();
+    }, 2000);
   }
 });
 
